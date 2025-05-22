70074b71e3bc4970b3620faec0993ed144f9f90a
jdrome <josedanilromero@gmail.com>
2025-05-22 08:45:53 -0500

    fix(repository): extend JpaRepository in CategoryRepository
    CategoryRepository was incorrectly implemented, it was missing the extension for the JpaRepository, so I added that part in the code

--- src/main/java/com/oroapp/oro/repository/CategoryRepository.java
+++ src/main/java/com/oroapp/oro/repository/CategoryRepository.java
@@ -1,9 +1,10 @@
 package com.oroapp.oro.repository;
 
 import com.oroapp.oro.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface CategoryRepository {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findByName(String name);
 }
