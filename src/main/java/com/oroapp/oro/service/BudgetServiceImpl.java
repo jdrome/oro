153efbf7e5d6982bda9ed89f530281173452cffb
jdrome <josedanilromero@gmail.com>
2025-05-22 22:53:12 -0500

    feat(service): Add BudgetServiceImpl.java code

--- src/main/java/com/oroapp/oro/service/BudgetServiceImpl.java
+++ src/main/java/com/oroapp/oro/service/BudgetServiceImpl.java
@@ -1,4 +1,37 @@
 package com.oroapp.oro.service;
 
-public class BudgetServiceImpl {
+import com.oroapp.oro.entity.Budget;
+import com.oroapp.oro.entity.Category;
+import com.oroapp.oro.entity.User;
+import com.oroapp.oro.repository.BudgetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BudgetServiceImpl implements BudgetService {
+
+    public final BudgetRepository budgetRepo;
+
+    @Autowired
+    public BudgetServiceImpl(BudgetRepository budgetRepo) {
+        this.budgetRepo = budgetRepo;
+    }
+
+    @Override
+    public Budget saveBudget(Budget budget) {
+        return budgetRepo.save(budget);
+    }
+
+    @Override
+    public List<Budget> getBudgetsByUser(User user) {
+        return budgetRepo.findByUser(user);
+    }
+
+    @Override
+    public Optional<Budget> getBudgetByUserAndCategory(User user, Category category) {
+        return budgetRepo.findByUserAndCategory(user, category);
+    }
 }
