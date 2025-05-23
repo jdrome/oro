9d3f9f1e4b25feaf6df9cca3c3139b1eb9feaa2d
jdrome <josedanilromero@gmail.com>
2025-05-22 22:32:53 -0500

    Build out UserService and UserServiceImpl

--- src/main/java/com/oroapp/oro/service/UserService.java
+++ src/main/java/com/oroapp/oro/service/UserService.java
@@ -5,7 +5,7 @@ import com.oroapp.oro.entity.User;
 import java.util.Optional;
 
 public interface UserService {
-    User save(User user);
+    User saveUser(User user);
     Optional<User> findByUsername(String username);
     Optional<User> findById(Long id);
 }
--- src/main/java/com/oroapp/oro/service/UserServiceImpl.java
+++ src/main/java/com/oroapp/oro/service/UserServiceImpl.java
@@ -1,4 +1,34 @@
 package com.oroapp.oro.service;
 
-public class UserServiceImpl {
+import com.oroapp.oro.entity.User;
+import com.oroapp.oro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepo;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.save(user);
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return userRepo.findByUsername(username);
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return userRepo.findById(id);
+    }
 }
