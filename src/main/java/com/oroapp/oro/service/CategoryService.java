ee9827a748c976cf47b0a5a07af92763048e8fcf
jdrome <josedanilromero@gmail.com>
2025-05-22 22:38:05 -0500

    feat(service): Add CategoryService.java code

--- src/main/java/com/oroapp/oro/service/CategoryService.java
+++ src/main/java/com/oroapp/oro/service/CategoryService.java
@@ -1,4 +1,12 @@
 package com.oroapp.oro.service;
 
+import com.oroapp.oro.entity.Category;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CategoryService {
+    Category saveCategory(Category category);
+    Optional<Category> findByName(String name);
+    List<Category> findAll();
 }
