af0ad748ae0aff3d500041da1170b75135843148
jdrome <josedanilromero@gmail.com>
2025-05-22 22:49:57 -0500

    feat(service): Add BudgetServce.java code

--- src/main/java/com/oroapp/oro/service/BudgetService.java
+++ src/main/java/com/oroapp/oro/service/BudgetService.java
@@ -1,4 +1,14 @@
 package com.oroapp.oro.service;
 
+import com.oroapp.oro.entity.Budget;
+import com.oroapp.oro.entity.Category;
+import com.oroapp.oro.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface BudgetService {
+    Budget saveBudget(Budget budget);
+    List<Budget> getBudgetsByUser(User user);
+    Optional<Budget> getBudgetByUserAndCategory(User user, Category category);
 }
