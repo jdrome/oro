7944528adaeb6ba33172af45b342eff7e30ab0a2
jdrome <josedanilromero@gmail.com>
2025-05-22 22:41:26 -0500

    feat(service): Add CategoryServiceImpl.java code

--- src/main/java/com/oroapp/oro/service/CategoryServiceImpl.java
+++ src/main/java/com/oroapp/oro/service/CategoryServiceImpl.java
@@ -1,4 +1,35 @@
 package com.oroapp.oro.service;
 
-public class CategoryServiceImpl {
+import com.oroapp.oro.entity.Category;
+import com.oroapp.oro.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepo;
+
+    @Autowired
+    public CategoryServiceImpl(CategoryRepository categoryRepo) {
+        this.categoryRepo = categoryRepo;
+    }
+
+    @Override
+    public Category saveCategory(Category category) {
+        return categoryRepo.save(category);
+    }
+
+    @Override
+    public Optional<Category> findByName(String name) {
+        return categoryRepo.findByName(name);
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return categoryRepo.findAll();
+    }
 }
