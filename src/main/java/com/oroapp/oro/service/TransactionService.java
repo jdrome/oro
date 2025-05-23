351c31c59b90ab119e7b790a5a610b41f70d2ada
jdrome <josedanilromero@gmail.com>
2025-05-22 22:43:38 -0500

    feat(service): Add TransactionService.java code

--- src/main/java/com/oroapp/oro/service/TransactionService.java
+++ src/main/java/com/oroapp/oro/service/TransactionService.java
@@ -1,4 +1,13 @@
 package com.oroapp.oro.service;
 
+import com.oroapp.oro.entity.Transaction;
+import com.oroapp.oro.entity.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TransactionService {
+    Transaction saveTransaction(Transaction transaction);
+    List<Transaction> getTransactionByUser(User user);
+    List<Transaction> getTransactionsBetweenDates(User user, LocalDate start, LocalDate end);
 }
